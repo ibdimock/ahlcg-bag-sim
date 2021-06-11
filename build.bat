ce8f491fefd4d9ee77d8e0361fbb704fd40123f9
ibdimock <ibdimock@gmail.com>
2021-06-11 16:33:19 -0400

    Update build script

--- build.bat
+++ build.bat
@@ -1,3 +1,23 @@
-cargo build --release --target web
+del /Q ..\ahlcg-bag-simulator-gh-pages\*
+rmdir /Q /S ..\ahlcg-bag-simulator-gh-pages\pkg
+rmdir /Q /S ..\ahlcg-bag-simulator-gh-pages\css
+rmdir /Q /S ..\ahlcg-bag-simulator-gh-pages\svgs
 
+cmd /C wasm-pack build --release --target web
+
+mkdir ..\ahlcg-bag-simulator-gh-pages\pkg\
 copy /Y pkg\*.* ..\ahlcg-bag-simulator-gh-pages\pkg\
+
+cmd /C sass .\ahlcg-simulator-frontend\scss\custom.scss .\ahlcg-simulator-frontend\css\custom.css
+cmd /C css-minify -f .\ahlcg-simulator-frontend\css\custom.css
+
+mkdir ..\ahlcg-bag-simulator-gh-pages\css\
+copy /Y .\css-dist\custom.min.css ..\ahlcg-bag-simulator-gh-pages\css\
+
+mkdir ..\ahlcg-bag-simulator-gh-pages\svgs\
+copy /Y .\ahlcg-simulator-frontend\svgs\* ..\ahlcg-bag-simulator-gh-pages\svgs\
+
+copy /Y .\ahlcg-simulator-frontend\index.html ..\ahlcg-bag-simulator-gh-pages\
+copy /Y .\ahlcg-simulator-frontend\chaos.png ..\ahlcg-bag-simulator-gh-pages\
+copy /Y .\ahlcg-simulator-frontend\simulator.js ..\ahlcg-bag-simulator-gh-pages\
+
